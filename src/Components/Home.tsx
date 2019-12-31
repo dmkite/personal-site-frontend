1377eac78a00ef73eddf6a65e147ed3fd0659cee
Dylan <kite.d92@gmail.com>
2019-12-30 17:09:31 -0800

    add svg

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,19 +1,30 @@
 import React, { Fragment } from 'react'
-import profileImage from '../assets/me.png'
+import {ReactComponent as Dylan} from '../assets/Dylan.svg'
+import {Link} from'react-router-dom'
 
 const Home = (): JSX.Element => {
   return (
     <Fragment>
       <h1>Dylan Kite</h1>
-      <p>
+<Dylan className='profile-image'/>      
+      <p className='intro-text'>
         I'm Dylan. I'm an artist and social worker turned full-stack JavaScript
 				engineer, a  commanding technical background in modern web technologies
 				complements meticulous communication and customer-forward thinking
 				honed as a non-profit educator. It’s my mission to develop, design, and
 				- above all - delight!
       </p>
-      <img className="profile-image" src={profileImage} alt="me" />
-
+      <section className='quick-links'>
+        <button>
+          <Link to='/development'>Development</Link>
+        </button>
+        <button>
+          <Link to='/design'>Design</Link>
+        </button>
+        <button>
+          <Link to='/gallery'>Gallery</Link>
+        </button>
+      </section>
     </Fragment>
   )
 }
