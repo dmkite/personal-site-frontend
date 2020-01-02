9a94c5d06d9b2906f48f7aa285f0c3208503a742
Kite <e48f@M-C02X73SVJHD2.nordstrom.net>
2020-01-02 12:50:22 -0800

    work on homepage css

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
-import {ReactComponent as Dylan} from '../assets/Dylan.svg'
-import {Link} from'react-router-dom'
+import { ReactComponent as Dylan } from '../assets/Dylan.svg'
+import { Link } from 'react-router-dom'
 
 const Home = (): JSX.Element => {
   return (
     <Fragment>
-      <h1>Dylan Kite</h1>
-<Dylan className='profile-image'/>      
+      <h1 className="title-font">Dylan Kite</h1>
+      <Dylan className='profile-image' />
       <p className='intro-text'>
         I'm Dylan. I'm an artist and social worker turned full-stack JavaScript
 				engineer, a  commanding technical background in modern web technologies
@@ -17,12 +17,15 @@ const Home = (): JSX.Element => {
       <section className='quick-links'>
         <button>
           <Link to='/development'>Development</Link>
+          <div className="trapezoid" />
         </button>
         <button>
-          <Link to='/design'>Design</Link>
+          <Link to='/design'>Graphic Design</Link>
+          <div className="trapezoid" />
         </button>
         <button>
-          <Link to='/gallery'>Gallery</Link>
+          <Link to='/gallery'>Art Gallery</Link>
+          <div className="trapezoid" />
         </button>
       </section>
     </Fragment>
